b78676026e531f3af058da88d15f8bd2a8e2e793
Georgy Dyuldin <g.dyuldin@gmail.com>
2024-09-01 12:35:56 +0300

    Update aether-x6100-control version

--- br2_external/package/aether-x6100-control/aether-x6100-control.mk
+++ br2_external/package/aether-x6100-control/aether-x6100-control.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-AETHER_X6100_CONTROL_VERSION = v0.15.0
-AETHER_X6100_CONTROL_SOURCE = $(AETHER_X6100_CONTROL_VERSION).tar.gz
-AETHER_X6100_CONTROL_SITE = https://github.com/AetherRadio/X6100Control/archive
+AETHER_X6100_CONTROL_VERSION = v0.15.2
+AETHER_X6100_CONTROL_SITE = https://github.com/gdyuldin/X6100Control
+AETHER_X6100_CONTROL_SITE_METHOD = git
 AETHER_X6100_CONTROL_LICENSE = GPLv2
 
 AETHER_X6100_CONTROL_INSTALL_STAGING = YES
