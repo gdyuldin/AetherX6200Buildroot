40db877056243f9b9d823b1e755690e36d1c794b
Georgy Dyuldin <g.dyuldin@gmail.com>
2024-10-13 23:02:36 +0300

    Add HW clock setup after ntp

--- br2_external/board/X6100/linux/rootfs-overlay/etc/NetworkManager/dispatcher.d/99-ntp-sync.sh
+++ br2_external/board/X6100/linux/rootfs-overlay/etc/NetworkManager/dispatcher.d/99-ntp-sync.sh
@@ -4,5 +4,6 @@ interface=$1 status=$2
 case "$status" in
   up)
     /usr/bin/ntpdate -u time.nist.gov >> /var/log/ntpdate.log  2>&1
+    hwclock -w -f /dev/rtc1 >> /var/log/ntpdate.log  2>&1
     ;;
 esac
