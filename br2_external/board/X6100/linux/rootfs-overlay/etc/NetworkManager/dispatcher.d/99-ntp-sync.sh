c3383863668f5ed27d4bd1d094168fd4b526373a
Georgy Dyuldin <g.dyuldin@gmail.com>
2024-10-15 18:52:43 +0300

    Add touch file after time sync

--- br2_external/board/X6100/linux/rootfs-overlay/etc/NetworkManager/dispatcher.d/99-ntp-sync.sh
+++ br2_external/board/X6100/linux/rootfs-overlay/etc/NetworkManager/dispatcher.d/99-ntp-sync.sh
@@ -3,7 +3,8 @@
 interface=$1 status=$2
 case "$status" in
   up)
-    /usr/bin/ntpdate -u time.nist.gov >> /var/log/ntpdate.log  2>&1
-    hwclock -w -f /dev/rtc1 >> /var/log/ntpdate.log  2>&1
+    /usr/bin/ntpdate -u time.nist.gov >> /var/log/ntpdate.log  2>&1 && \
+    hwclock -w -f /dev/rtc1 >> /var/log/ntpdate.log  2>&1 && \
+    touch /var/run/time-sync
     ;;
 esac
