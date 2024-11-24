8161f08aa3bb0c659ee672314bbf1a587acf01a4
Georgy Dyuldin <g.dyuldin@gmail.com>
2024-11-24 15:01:04 +0300

    Add webserver

---
+++ br2_external/package/x6100-webserver/x6100_webserver.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+#  X6100 GUI
+#
+################################################################################
+
+X6100_WEBSERVER_VERSION = v0.0.1
+# X6100_WEBSERVER_SITE = https://github.com/gdyuldin/x6100_webserver
+# X6100_WEBSERVER_SITE_METHOD = git
+# X6100_WEBSERVER_GIT_SUBMODULES = YES
+X6100_WEBSERVER_SITE = /home/tush/workspace/radio_calc/x6100/x6100_webserver
+X6100_WEBSERVER_SITE_METHOD = local
+X6100_WEBSERVER_LICENSE = GPLv2
+X6100_WEBSERVER_DEPENDENCIES = python-bottle-sqlite
+X6100_WEBSERVER_SETUP_TYPE = flit
+
+$(eval $(python-package))
