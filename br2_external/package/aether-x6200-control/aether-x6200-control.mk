719b0201b9c8f672cbc1f24a008a1232f7bcb9c3
Georgy Dyuldin <g.dyuldin@gmail.com>
2025-03-30 16:09:25 +0300

    Update config

--- br2_external/package/aether-x6200-control/aether-x6200-control.mk
+++ br2_external/package/aether-x6200-control/aether-x6200-control.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AETHER_X6200_CONTROL_VERSION = v0.15.6
+AETHER_X6200_CONTROL_VERSION = v0.1.0
 AETHER_X6200_CONTROL_SITE = https://github.com/gdyuldin/X6200Control
 AETHER_X6200_CONTROL_SITE_METHOD = git
 AETHER_X6200_CONTROL_LICENSE = GPLv2
--- br2_external/package/x6200-gui/x6200_gui.mk
+++ br2_external/package/x6200-gui/x6200_gui.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-X6200_GUI_VERSION = v0.23.0-rc.3
+X6200_GUI_VERSION = v0.1.0
 X6200_GUI_SITE = https://github.com/gdyuldin/x6200_gui
 X6200_GUI_SITE_METHOD = git
 X6200_GUI_GIT_SUBMODULES = YES
